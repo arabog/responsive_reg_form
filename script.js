e94c0cc76cbe17bca2aeeb7c5abfedad2140941b
arabog <arababs@gmail.com>
2022-06-17 09:05:02 +0100

    update 2

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const first = document.querySelector(".first");
-const first_name = document.querySelector(".first_name").value;
+const first_name = document.querySelector(".first_name");
 
 const last = document.querySelector(".last");
+const last_name = document.querySelector(".last_name").value;
 
 const email = document.querySelector(".email");
+const user_email = document.querySelector(".user_email").value;
 
 const password = document.querySelector(".password");
-
+const user_password = document.querySelector(".user_password").value;
 
 let btn = document.querySelector(".btn")
 
-console.log(first_name)
+console.log(first_name.value.length())
 
-// first_name.addEventListener("click", displayDate)
+// console.log(last_name)
 
-function displayDate() {
-    alert("Hello")
+// console.log(user_email)
+
+// console.log(user_password)
+
+
+btn.addEventListener("click", displayDate)
+
+function displayDate(first_name) {
+    if(first_name.value.length() == 0) {
+        alert("empty")
+    }
+
 }
