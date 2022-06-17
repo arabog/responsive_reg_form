f6faf983c410f26924db4597f42dc785cd53a102
arabog <arababs@gmail.com>
2022-06-17 09:48:55 +0100

    update   3

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-const first = document.querySelector(".first");
 const first_name = document.querySelector(".first_name");
+const first = document.querySelector(".first");
 
-const last = document.querySelector(".last");
 const last_name = document.querySelector(".last_name").value;
+// const last = document.querySelector(".last");
 
-const email = document.querySelector(".email");
 const user_email = document.querySelector(".user_email").value;
+// const email = document.querySelector(".email");
 
-const password = document.querySelector(".password");
 const user_password = document.querySelector(".user_password").value;
+// const password = document.querySelector(".password");
 
 let btn = document.querySelector(".btn")
 
-console.log(first_name.value.length())
+// console.log(first_name.value == "")
 
-// console.log(last_name)
+// user_email.addEventListener("keyup", function (event) {
+//     if (user_email.validity.typeMismatch) {
+//       user_email.setCustomValidity("I expect an e-mail, darling!");
+//     } else {
+//       user_email.setCustomValidity("");
+//     }
+//   })
 
-// console.log(user_email)
-
-// console.log(user_password)
 
 
 btn.addEventListener("click", displayDate)
 
-function displayDate(first_name) {
-    if(first_name.value.length() == 0) {
-        alert("empty")
-    }
+function displayDate() {
 
+    // setTimeout(() => {
+        if(first_name.value == "") {
+    
+            const msg = first.innerHTML = "First Name cannot be empty";
+            console.log(msg)
+            // alert("hello")
+        }
+
+    // }, 3000)
+    
 }
