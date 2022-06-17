4e7b71933240828ecc9bf8048706ee9e626c95a4
arabog <arababs@gmail.com>
2022-06-17 08:47:43 +0100

    update 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello")
+const first = document.querySelector(".first");
+const first_name = document.querySelector(".first_name").value;
+
+const last = document.querySelector(".last");
+
+const email = document.querySelector(".email");
+
+const password = document.querySelector(".password");
+
+
+let btn = document.querySelector(".btn")
+
+console.log(first_name)
+
+// first_name.addEventListener("click", displayDate)
+
+function displayDate() {
+    alert("Hello")
+}
