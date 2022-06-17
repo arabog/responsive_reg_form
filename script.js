b0815f9c7a2750d0f9fa79529913a337dbc20249
arabog <arababs@gmail.com>
2022-06-17 11:11:20 +0100

    update   4

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
-const first_name = document.querySelector(".first_name");
-const first = document.querySelector(".first");
 
-const last_name = document.querySelector(".last_name").value;
-// const last = document.querySelector(".last");
 
-const user_email = document.querySelector(".user_email").value;
-// const email = document.querySelector(".email");
-
-const user_password = document.querySelector(".user_password").value;
-// const password = document.querySelector(".password");
 
 let btn = document.querySelector(".btn")
-
-// console.log(first_name.value == "")
-
-// user_email.addEventListener("keyup", function (event) {
-//     if (user_email.validity.typeMismatch) {
-//       user_email.setCustomValidity("I expect an e-mail, darling!");
-//     } else {
-//       user_email.setCustomValidity("");
-//     }
-//   })
-
 
 
 btn.addEventListener("click", displayDate)
 
 function displayDate() {
+    const first_name = document.querySelector(".first_name");
+    const first = document.querySelector(".first");
+    first.classList.add("first");
 
-    // setTimeout(() => {
-        if(first_name.value == "") {
-    
-            const msg = first.innerHTML = "First Name cannot be empty";
-            console.log(msg)
-            // alert("hello")
-        }
+    const error1 = document.querySelector(".error1")
+    error1.classList.add("error1")
 
-    // }, 3000)
+
+    const last_name = document.querySelector(".last_name");
+    const last = document.querySelector(".last");
+    last.classList.add("first")
+
+    const error2 = document.querySelector(".error2")
+    error2.classList.add("error2")
     
+
+    const user_email = document.querySelector(".user_email");
+    const email = document.querySelector(".email");
+    email.classList.add("first")
+
+    const user_password = document.querySelector(".user_password");
+    const password = document.querySelector(".password");
+    password.classList.add("first")
+
+    const error3 = document.querySelector(".error3")
+    error3.classList.add("error3")
+
+
+    if(first_name.value == "") {
+            error1.style.display = "block";
+
+            first.innerHTML = "First Name cannot be empty";
+    }
+
+    if(last_name.value == "") {
+        error2.style.display = "block";
+
+        last.innerHTML =   "Last Name cannot be empty";
+    }
+
+    // if(user_email.value == "") {
+
+    //     email.innerHTML =   "Last Name cannot be empty";
+    // }
+
+    if(user_password.value == "") {
+        error3.style.display = "block";
+
+        password.innerHTML =   "Password cannot be empty";
+    }    
 }
